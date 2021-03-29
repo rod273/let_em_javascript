fd44f174797783f50ea02aabadf3f67844092787
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-03-29 20:24:42 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let nome = 'joão';
+
+
+console.log(nome , ' nasceu em 1985.');
+console.log('em 200 joao conheceu a grazi.');
+console.log('joao casou com a grazi em 2022.');
+console.log('grazi teve um filho de joao.');
+console.log('o filho de' , nome , 'chama rodrigo.');
